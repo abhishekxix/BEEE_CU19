df4c5dcce11e45f901a4f0665ff203e8f11a6e90
abhisingh127 <48520239+abhisingh127@users.noreply.github.com>
2019-08-30 22:31:53 +0530

    Update LED_Flasher_code.ino

--- Exp1_LED_Flasher/LED_Flasher_code.ino
+++ Exp1_LED_Flasher/LED_Flasher_code.ino
@@ -1,9 +1,9 @@
 //This is the required sketch to perform the LED flasher experiment.
 void setup()
 { for(int i=0;i<4;i++) 
-{
-  pinMode(i, OUTPUT);
-}
+  {
+    pinMode(i, OUTPUT);
+  }
 }
 
 void loop()
